46e3928f663218682c71fef26377c1df87af8035
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-07-20 14:39:06 +0600

    feat: sign in page complete

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,86 @@
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import colors from "../../constants/colors";
+import { router } from "expo-router";
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <Image
+          source={require("../../assets/images/logo.png")}
+          style={{ width: 180, height: 180 }}
+          resizeMode="contain"
+        />
+        <Text style={styles.heading}>Sign In to Your Account</Text>
+        
+        <TextInput
+          placeholder="Enter Your Email"
+          placeholderTextColor={colors.LIGHT_GRAY}
+          style={styles.textInput}
+          keyboardType="email-address"
+        />
+        <TextInput
+          placeholder="Enter Your Password"
+          placeholderTextColor={colors.LIGHT_GRAY}
+          style={styles.textInput}
+          secureTextEntry
+        />
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+
+        <View style={{ flexDirection: "row", gap: 5, marginTop: 20 }}>
+          <Text style={{ fontFamily: "outfit" }}>Don't have an account?</Text>
+          <TouchableOpacity onPress={() => router.push("/sign-up")}>
+            <Text style={{ color: colors.PRIMARY ,fontFamily: "outfit-bold"}}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({
+  scrollView: {
+    alignItems: "center",
+    paddingTop: 80,
+    backgroundColor: colors.WHITE,
+    padding: 25,
+    minHeight: "100%",
+  },
+  heading: {
+    fontSize: 30,
+    fontFamily: "outfit-bold",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: colors.GRAY,
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: colors.PRIMARY,
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+    width: "100%",
+  },
+  buttonText: {
+    color: colors.WHITE,
+    fontSize: 20,
+    fontFamily: "outfit",
+    textAlign: "center",
+  },
+});
