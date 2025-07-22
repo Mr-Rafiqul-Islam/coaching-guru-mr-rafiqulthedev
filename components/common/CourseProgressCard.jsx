ced86d4a5de3f154bbd91e07e95086580f973380
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-07-22 18:44:52 +0600

    Update: Course Progress

---
+++ components/common/CourseProgressCard.jsx
@@ -0,0 +1,54 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import { imageAssets } from "../../constants/option";
+import * as Progress from "react-native-progress";
+import colors from "../../constants/colors";
+
+const CourseProgressCard = ({ item, containerWidth }) => {
+  return (
+    <View style={[styles.courseContainer, { width: containerWidth }]}>
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 5,
+          gap: 10,
+        }}
+      >
+        <Image
+          source={imageAssets[item.banner_image]}
+          style={{ width: 80, height: 80, borderRadius: 15 }}
+          resizeMode="cover"
+        />
+
+        <View style={{ flex: 1 }}>
+          <Text
+            style={{ fontFamily: "outfit-bold", flexWrap: "wrap" }}
+            numberOfLines={2}
+          >
+            {item?.courseTitle}
+          </Text>
+          <Text style={{ fontFamily: "outfit", margingTop: 5 }}>
+            {item?.chapters?.length} Chapters
+          </Text>
+        </View>
+      </View>
+      <View style={{ marginTop: 10 }}>
+        <Progress.Bar progress={1 / 3} width={containerWidth-20} />
+        <Text style={{ fontFamily: "outfit", marginTop: 5 }}>
+          3 Out of 5 Chapter Completed
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default CourseProgressCard;
+
+const styles = StyleSheet.create({
+  courseContainer: {
+    padding: 10,
+    backgroundColor: colors.BG_GRAY,
+    borderRadius: 15,
+    margin: 6,
+    borderRadius: 15,
+  },
+});
--- components/home/CourseProgress.jsx
+++ components/home/CourseProgress.jsx
@@ -1,8 +1,5 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import { Image } from "react-native";
-import { imageAssets } from "../../constants/option";
-import colors from "../../constants/colors";
-import * as Progress from 'react-native-progress';
+import { FlatList, Text, View } from "react-native";
+import CourseProgressCard from "../common/CourseProgressCard";
 
 const CourseProgress = ({ courseList }) => {
   return (
@@ -13,36 +10,8 @@ const CourseProgress = ({ courseList }) => {
         horizontal={true}
         showsHorizontalScrollIndicator={false}
         renderItem={({ item, index }) => (
-          <View key={index} style={styles.courseContainer}>
-            <View
-              style={{
-                flexDirection: "row",
-                marginTop: 5,
-                gap: 10,
-              }}
-            >
-              <Image
-                source={imageAssets[item.banner_image]}
-                style={{ width: 80, height: 80, borderRadius: 15 }}
-                resizeMode="cover"
-              />
-
-              <View>
-                <Text style={{ fontFamily: "outfit-bold",}} numberOfLines={1}>
-                  {item?.courseTitle}
-                </Text>
-                <Text style={{ fontFamily: "outfit", margingTop: 5 }}>
-                  {item?.chapters?.length} Chapters
-                </Text>
-              </View>
-            </View>
-            <View style={{ marginTop: 10 }}>
-
-            <Progress.Bar progress={1/3} width={220} />
-            <Text style={{ fontFamily: "outfit", marginTop: 5 }}>
-                3 Out of 5 Chapter Completed
-            </Text>
-            </View>
+          <View key={index}>
+            <CourseProgressCard item={item} containerWidth={240}/>
           </View>
         )}
       />
@@ -52,13 +21,3 @@ const CourseProgress = ({ courseList }) => {
 
 export default CourseProgress;
 
-const styles = StyleSheet.create({
-  courseContainer: {
-    padding: 10,
-    backgroundColor: colors.BG_GRAY,
-    borderRadius: 15,
-    margin: 6,
-    borderRadius: 15,
-    width: 240,
-  },
-});
